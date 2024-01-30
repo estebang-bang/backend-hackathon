6587bc21936186e53e8f58d505e5e2489c4faf3a
PeterHdz7 <hernandezpeter38230@gmail.com>
2024-01-30 17:33:36 +0100

    creation route first post et first get

--- routes/trips.js
+++ routes/trips.js
@@ -8,7 +8,7 @@ router.get("/", (req, res) => {
   Trip.findOne({
     departure: { $regex: new RegExp(req.body.departure, "i") },
     arrival: { $regex: new RegExp(req.body.arrival, "i") } ,
-    date: req.body.date.getTime()
+    //date: req.body.date.getTime()
   }).then((data) => {
       res.json({ trips: data });
   });
