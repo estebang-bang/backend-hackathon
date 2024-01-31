e622062f13d9c91f562d94ec52dcf25ad15d2946
PeterHdz7 <hernandezpeter38230@gmail.com>
2024-01-31 09:08:06 +0100

    POST CREATION BOOKING

--- routes/booking.js
+++ routes/booking.js
@@ -1,6 +1,16 @@
 var express = require('express');
 var router = express.Router();
+const Booking = require('../models/bookings');
 
-
+router.post("/", (req, res) => {
+    const newBooking = new Booking({
+      departure: req.body.departure,
+      arrival: req.body.arrival,
+      // date: req.body.date.getTime(),
+    });
+    newBooking.save().then((newDoc) => {
+      res.json({ newDoc: newDoc });
+    });
+  });
 
 module.exports = router;
